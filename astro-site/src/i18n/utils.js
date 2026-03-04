90d70b079811bc5da42e6cb3ab9e3cf0b97d2baf
Jini Francis <17831956+jinifrancis@users.noreply.github.com>
2026-03-04 14:57:24 +0530

    Add language switcher for Malayalam version of website
    * Add available translations for malayalam language

---
+++ astro-site/src/i18n/utils.js
@@ -0,0 +1,19 @@
+import { ui, defaultLang } from './ui';
+
+export function getLangFromUrl(url) {
+  const [, lang] = url.pathname.split('/');
+  if (lang in ui) return lang;
+  return defaultLang;
+}
+
+export function useTranslations(lang) {
+  return function t(key) {
+    return ui[lang][key] || ui[defaultLang][key];
+  }
+}
+
+export function useTranslatedPath(lang) {
+  return function translatePath(path, l = lang) {
+    return l === defaultLang ? path : `/${l}${path}`;
+  }
+}
